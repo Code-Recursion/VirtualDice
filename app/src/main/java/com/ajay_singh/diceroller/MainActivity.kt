7333936b22363d3e170ae1743c408434498c2dda
Ajay Singh <helloajaysingh1@gmail.com>
2020-05-17 19:01:28 +0530

    Add one more dice

--- app/src/main/java/com/ajay_singh/diceroller/MainActivity.kt
+++ app/src/main/java/com/ajay_singh/diceroller/MainActivity.kt
@@ -6,36 +6,40 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
-
-
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var diceImage : ImageView
+    private lateinit var diceImage1 : ImageView
+    private lateinit var diceImage2 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
 
         val rollButton : Button = findViewById(R.id.roll_button)
+        val clearButton : Button = findViewById(R.id.clear_button)
 
         rollButton.setOnClickListener{
             rollDice()
         }
 
+        clearButton.setOnClickListener{
+            clearDice()
+        }
     }
 
-
-
     private fun rollDice() {
-        var count:Int = 0
-        val randomInt = (1..6).random()
-        
-        val resultText:TextView = findViewById(R.id.resultText)
 
-        val drawableResource = when (randomInt) {
+        val resultText1 : TextView = findViewById(R.id.textView1)
+        val resultText2 : TextView = findViewById(R.id.textView2)
+
+        var count : Int = 0
+        count++
+        var randomOne : Int = (1..6).random()
+        val drawableResource1 = when (randomOne) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -43,11 +47,30 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        count++
-        diceImage.setImageResource(drawableResource)
+
         if(count > 0) {
-            resultText.text = ("You got : " + randomInt).toString()
+            resultText1.text = randomOne.toString()
         }
+        diceImage1.setImageResource(drawableResource1)
+
+        var randomTwo : Int = (1..6).random()
+        val drawableResource2 = when(randomTwo) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        if(count > 0) {
+            resultText2.text = randomTwo.toString()
+        }
+        diceImage2.setImageResource(drawableResource2)
+
     }
 
+    private fun clearDice() {
+        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
 }
